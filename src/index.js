45de3e2e5546233a49a9170f6c694d973c15dd5a
Reyser Zapata <rzapata@unsa.edu.pe>
2023-05-29 10:57:06 -0500

    Listar archivos terminado

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
+const app = express();
 
-const app = express()
-app.use(express.static('../public'))
-
+app.use(express.static("../public"));
+app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000');
-})
+  console.log("Servidor escuchando en el puerto 3000");
+});
 
-// Routers
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-app.get('/archivos', (req, res) => {
-    res.send("Archivos")
-})
+app.get("/archivos", (req, res) => {
+  fs.readdir(__dirname + "/markdown", (err, files) => {
+    if (err) {
+      console.error(err);
+      return res.status(404).json({
+        error: "No se encontró la carpeta deseada",
+      });
+    }
 
-app.get('/archivos/:id', (req, res) => {
-    res.send('Archivo ' + req.params.id);
-})
+    const markdownFiles = files.filter((file) => path.extname(file) === '.md');
+    res.json(markdownFiles);
+    console.log(markdownFiles);
+  });
+});
 
-app.get('/crear', (req, res) => {
-    res.send('Crear');
-})
+app.get("/archivos/:name", (req, res) => {
+
+
+});
+
+app.get("/crear", (req, res) => {
+  res.send("Crear");
+});
 
 // Route NotFound
 app.use((req, res) => {
-    res.status(404).send('No se encontró la Página')
-})
+  res.status(404).send("No se encontró la Página");
+});
