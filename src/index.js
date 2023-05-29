b249bc89dee6b848e59cdebbb8ac74983dd051ea
Reyser Zapata <rzapata@unsa.edu.pe>
2023-05-29 11:08:59 -0500

    Leer y mostrar archivo específico culminado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const markdownIt = require('markdown-it');
 
 const app = express();
+const md = new markdownIt()
 
 app.use(express.static("../public"));
 app.use(express.json());
@@ -20,7 +22,7 @@ app.get("/archivos", (req, res) => {
   fs.readdir(__dirname + "/markdown", (err, files) => {
     if (err) {
       console.error(err);
-      return res.status(404).json({
+      return res.status(500).json({
         error: "No se encontró la carpeta deseada",
       });
     }
@@ -32,9 +34,19 @@ app.get("/archivos", (req, res) => {
 });
 
 app.get("/archivos/:name", (req, res) => {
+    const name = req.params.name
+    const filePath = path.join(__dirname + '/markdown/', name)
+    
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err){
+            console.error(err)
+            return res.status(500).json({error: 'Error al leer el archivo'})
+        }
 
-
-});
+        const html = md.render(data)
+        res.send(html)
+    })
+})
 
 app.get("/crear", (req, res) => {
   res.send("Crear");
