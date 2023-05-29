f58a95e81e9c07f214b91006ec9a9fd76c704c2a
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-05-29 17:59:25 -0500

    Metodo Crear y Guardar archivo hecho

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 document.getElementById("show-files").addEventListener("click", function () {
-  const xhr = new XMLHttpRequest(); // Peticion AJAX
-  xhr.open("GET", "/archivos", true);
+  document.getElementById("Markdown-form").style.display = "none"
+
+  const xhr = new XMLHttpRequest() // Peticion AJAX
+  xhr.open("GET", "/archivos", true)
 
   xhr.onreadystatechange = (err) => {
     if (xhr.status === 200) {
@@ -11,21 +13,52 @@ document.getElementById("show-files").addEventListener("click", function () {
 
       for (var i = 0; i < response.length; i++) {
         const fileName = response[i];
-        const link = document.createElement("a");
+        const link = document.createElement("a")
 
         link.href = "/markdown/" + fileName; // Ruta a los archivos markdown
-        link.textContent = fileName;
+        link.textContent = fileName
 
-        boxContainer.appendChild(link);
-        boxContainer.appendChild(document.createElement("br"));
+        boxContainer.appendChild(link)
+        boxContainer.appendChild(document.createElement("br"))
       }
     } else {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
-  xhr.send();
+  xhr.send()
+})
+
+document.getElementById("crate-new").addEventListener("click", function () {
+  document.getElementById("Markdown-form").style.display = "block"
+  document.getElementById("box-container").innerHTML=""
 });
 
+document
+  .getElementById("Markdown-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault()
 
+    const titulo = document.getElementById("titulo").value;
+    const texto = document.getElementById("texto").value;
 
+    const xhr = new XMLHttpRequest()
+    xhr.open("POST", "/archivos", true)
+    xhr.setRequestHeader("Content-Type", "application/json")
+
+    xhr.onreadystatechange = function () {
+      if (xhr.status === 200) {
+        console.log("Archivo creado y guardado exitosamente")
+        // Realizar cualquier acción adicional después de guardar el archivo
+      } else {
+        console.error("Error al crear y guardar el archivo:", xhr.status)
+      }
+    };
+
+    var data = {
+      titulo: titulo,
+      texto: texto,
+    };
+
+    xhr.send(JSON.stringify(data))
+  });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const markdownIt = require('markdown-it');
+const markdownIt = require("markdown-it");
 
 const app = express();
-const md = new markdownIt()
+const md = new markdownIt();
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -18,7 +18,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-
 // Mostrar la lista de archivos markdown que hay en la carpeta
 app.get("/archivos", (req, res) => {
   fs.readdir(__dirname + "/markdown", (err, files) => {
@@ -29,7 +28,7 @@ app.get("/archivos", (req, res) => {
       });
     }
 
-    const markdownFiles = files.filter((file) => path.extname(file) === '.md');
+    const markdownFiles = files.filter((file) => path.extname(file) === ".md");
     res.json(markdownFiles);
     console.log(markdownFiles);
   });
@@ -37,23 +36,47 @@ app.get("/archivos", (req, res) => {
 
 // Visualizar archivo Markdown en HTML
 app.get("/markdown/:name", (req, res) => {
-    const name = req.params.name
-    const filePath = path.join(__dirname + '/markdown/', name)
-    
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err){
-            console.error(err)
-            return res.status(500).json({error: 'Error al leer el archivo'})
-        }
+  const name = req.params.name;
+  const filePath = path.join(__dirname + "/markdown/", name);
 
-        const html = md.render(data)
-        res.send(html)
-    })
-})
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error al leer el archivo" });
+    }
 
+    const html = md.render(data);
+    res.send(html);
+  });
+});
 
-app.get("/crear", (req, res) => {
-  res.send("Crear");
+app.post("/crear", (req, res) => {
+  const { titulo, texto } = req.body;
+
+  // Verificar si la carpeta "markdown" existe, si no, crearla
+  const directorioMarkdown = path.join(__dirname, "markdown");
+  if (!fs.existsSync(directorioMarkdown)) {
+    fs.mkdirSync(directorioMarkdown);
+  }
+
+  const nombreArchivo = `${titulo}.md`;
+  const rutaArchivo = path.join(directorioMarkdown, nombreArchivo);
+
+  // Verificar si el archivo ya existe
+  if (fs.existsSync(rutaArchivo)) {
+    return res.status(200).json({ confirmar: true });
+  }
+
+  // Guardar el contenido del archivo Markdown
+  fs.writeFile(rutaArchivo, texto, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error al guardar el archivo" });
+    }
+
+    console.log("Archivo Markdown guardado exitosamente");
+    res.status(200).json({ message: "Archivo Markdown creado y guardado" });
+  });
 });
 
 // Route NotFound
